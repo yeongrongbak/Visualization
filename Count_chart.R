d278035cf06450c776bf8ff494c7b998d7672e8c
Yeongrong Bak <yeongrong.bak@gmail.com>
2018-10-09 23:18:48 +0900

    Add files via upload

---
+++ Count_chart.R
@@ -0,0 +1,12 @@
+# load package and data
+library(ggplot2)
+data(mpg, package="ggplot2")
+
+# Scatterplot
+theme_set(theme_bw())  # pre-set the bw theme.
+g <- ggplot(mpg, aes(cty, hwy))
+g + geom_count(col="tomato3", show.legend=F) +
+  labs(subtitle="mpg: city vs highway mileage", 
+       y="hwy", 
+       x="cty", 
+       title="Counts Plot")
