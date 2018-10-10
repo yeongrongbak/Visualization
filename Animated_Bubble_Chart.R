47a3aed980f3edd22f9209b1c2ac4debed3193ac
Yeongrong Bak <yeongrong.bak@gmail.com>
2018-10-10 22:55:41 +0900

    Add files via upload

---
+++ Animated_Bubble_Chart.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(gganimate)
+library(gapminder)
+theme_set(theme_bw())  # pre-set the bw theme.
+
+g <- ggplot(gapminder, aes(gdpPercap, lifeExp, size = pop, frame = year)) +
+  geom_point() +
+  geom_smooth(aes(group = year), 
+              method = "lm", 
+              show.legend = FALSE) +
+  facet_wrap(~continent, scales = "free") +
+  scale_x_log10()  # convert to log scale
+
+gganimate(g, interval=0.2)
